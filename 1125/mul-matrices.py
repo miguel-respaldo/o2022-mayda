85db6231e9f56ce2dbedf6bbe1eb9208b2214bb5
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2022-11-25 21:30:30 -0600

    fixes

--- 1125/mul-matrices.py
+++ 1125/mul-matrices.py
@@ -8,12 +8,12 @@ m3 = list()
 def multiplicacion(a, b):
     ret = list()
     for f in range(len(a)):
+        ret.append(list())
         for c in range(len(b[f])):
-            ret.append(list())
             suma = 0
             for m in range(len(a[f])):
                 suma = a[f][m] * b[m][c]
-            ret[c].append(suma)
+            ret[f].append(suma)
     return ret
 
 f1 = int(input("Filas de la primera matriz: "))
@@ -21,7 +21,7 @@ c1 = int(input("Columnas de la primera matriz: "))
 f2 = int(input("Filas de la segunda matriz: "))
 c2 = int(input("Columnas de la segunda matriz: "))
 
-if c1 != f1:
+if c1 != f2:
     print("Columnas de la primera y filas de la segunda matriz no coinciden")
     os._exit(1)
 
@@ -39,18 +39,21 @@ for f in range(f2):
 
 m3 = multiplicacion(m1,m2)
 
+print("M1")
 for f in range(f1):
     print("[ ", end="")
     for c in range(c1):
         print(m1[f][c], end=", ")
     print("]")
 
+print("M2")
 for f in range(f2):
     print("[ ", end="")
     for c in range(c2):
         print(m2[f][c], end=", ")
     print("]")
 
+print("M3")
 for f in range(len(m3)):
     print("[ ", end="")
     for c in range(len(m3[f])):
